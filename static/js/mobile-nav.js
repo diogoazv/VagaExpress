32b097d9dcfd382c0fe5b7515adf8f7b57e75ab1
diogoazv <diogoazz07@gmail.com>
2026-04-13 06:58:45 -0300

    Implementaçao da Biblioteca Gsap js, e tela de criar perfil

--- static/js/mobile-nav.js
+++ static/js/mobile-nav.js
@@ -6,3 +6,46 @@
         }
         itens_mobile.forEach((item) =>
             item.addEventListener('click', activeLink));
+
+
+
+
+// Filtro mobile
+
+const mostrar_filtros = document.querySelector('.ver_filtros');
+const filtro_mobile = document.querySelector('.filtros-mobile');
+const texto_exibir = document.querySelector('.exibir');
+
+let aberto = false;
+
+mostrar_filtros.addEventListener('click', function() {
+
+    if (!aberto) {
+        // MOSTRAR
+        filtro_mobile.style.display = 'flex';
+
+        gsap.fromTo(filtro_mobile,
+            { y: -50, opacity: 0 },
+            { y: 0, opacity: 1, duration: 0.4, ease: "power2.out" }
+        );
+
+        texto_exibir.textContent = 'Ocultar';
+        aberto = true;
+
+    } else {
+        // ESCONDER
+        gsap.to(filtro_mobile, {
+            y: -50,
+            opacity: 0,
+            duration: 0.3,
+            ease: "power2.in",
+            onComplete: () => {
+                filtro_mobile.style.display = 'none';
+            }
+        });
+
+        texto_exibir.textContent = 'Exibir';
+        aberto = false;
+    }
+
+});
